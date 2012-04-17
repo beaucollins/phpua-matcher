c5594568bee6fe681e2763157ed9e1184ccd77b6
Beau Collins <beaucollins@gmail.com>
2012-04-17 15:25:29 -0700

    Pretty complete implementation for what is needed

--- Classes/WPCOM_Platform_Redirector.php
+++ Classes/WPCOM_Platform_Redirector.php
@@ -1,24 +1,92 @@
 <?php
-
-
-class WPCOM_Platform_Redirector implements Iterator {
+/**
+ * This class builds an array of Closures and their corresponding URL's
+ * and labels and then provides a method to match a User Agent string by
+ * invoking each closure in the order that they are configured.
+ *
+ * Usage:
+ *    
+ *   // instantiate and configure the instance
+ *	 $redirector = new WPCOM_Platform_Redirector();
+ *
+ *   // provide a single regex to compare against the ua
+ *   $redirector->on( "/ios/i", 'http://www.somewhere.com', 'Take me somewhere' );
+ * 
+ *   // provide a closure to use to examine the ua to do some fancy comparing
+ *   $redirector->on( function( $ua ){
+ *     if ( strlen( $ua ) > 10 && preg_match( "/something/i", $ua ) ) {
+ *       return true;
+ *     }
+ *     return false;
+ *   }, 'http://somewhereelse.com', 'Take me somewhere else' );
+ *
+ *   // and it's chainable too
+ *   $redirector->on( "/[\d]+/", "http://digitsareus.com", "We've got digits!" )
+ *              ->on( "/wordpress/i", "http://wordpress.com", "You're using WordPress" )
+ *              ->on( "/(one|two|three)/i", "http://numbers.com", "You can spell numbers" );
+ */
+class WPCOM_Platform_Redirector {
 
 	var $matchers = array();
 	
 	private static $instance;
-	private $index = 0;
 	
+	/**
+	 * @return WPCOM_Platform_Redirector instance
+	*/
 	static function getInstance(){
 		if ( self::$instance === null ) {
 			self::$instance = new WPCOM_Platform_Redirector();
 		}
 		return self::$instance;
 	}
+		
+	/**
+	 * Finds the first matching item for the given UA string
+	 *
+	 * @param  string $ua
+	 * @return Array
+	*/
+	public function matching( $ua ){
+		$match = $this->find( function( $matcher ) use ( $ua ) {
+			return $matcher['closure']->__invoke( $ua );
+		} );		
+		return is_array( $match ) ? $match : array();
+	}
 	
+	/**
+	 * If $closure is a string it wraps it into a Closure that uses preg_match
+	 *
+	 * @param  mixed  $closure
+	 * @return Closure 
+	*/
+	private function prepareClosure( $closure ){
+		
+		if ( is_string( $closure ) ) {
+			$compare = $closure;
+			$closure = function( $ua ) use ( $compare ){
+				return preg_match( $compare, $ua );
+			};
+		}
+		
+		return $closure;
+	}
+	
+	/**
+	 * Adds a regex/closure/invokable object and matching $href and $label
+	 * to the end of the list
+	 *
+	 * @param  mixed  $closure
+	 * @param  string   $href
+	 * @param  string   $label
+	 * @return WPCOM_Platform_Redirector 
+	*/
 	public function on( $closure, $href, $label ){
 		
+		$matcher = $this->prepareClosure( $closure );
+		
 		array_push( $this->matchers, array(
-			'closure' => $closure,
+			'closure' => $matcher,
 			'href' => $href,
 			'label' => $label
 		) );
@@ -27,27 +95,78 @@ class WPCOM_Platform_Redirector implements Iterator {
 		
 	}
 	
+	/**
+	 * Adds a closure to test the UA against as the beginning of the list
+	 *
+	 * @param  mixed  $closure
+	 * @param  string   $href
+	 * @param  string   $label
+	 * @return WPCOM_Platform_Redirector 
+	*/
+	public function prepend( $closure, $href, $label ){
+		
+		$matcher = $this->prepareClosure( $closure );
+		
+		array_unshift( $this->matchers, array(
+			'closure' => $matcher,
+			'href' => $href,
+			'label' => $label
+		) );
+			
+		return $this;
+		
+	}
+	
+	/**
+	 * Invokes the $closure witch each item in the matcher array
+	 *
+	 * @param  Closure  $closure
+	 * @return void 
+	*/
 	public function each( $closure ){
 		foreach( $this->matchers as $item ){
 			$closure->__invoke( $item );
 		}
 	}
 	
+	/**
+	 * Invokes the $closure witch each item in the matcher array
+	 * and returns an array that conists of the return values
+	 * from $closure
+	 *
+	 * @param  Closure  $closure
+	 * @return Array 
+	*/
 	public function map( $closure ){
 		$mapped = array();
-		foreach( $this->matchers as $item ){
+		$this->each( function( $item ) use ( &$mapped, $closure ) {
 			array_push( $mapped, $closure->__invoke( $item ) );
-		}
+		} );
 		return $mapped;
 	}
 	
+	/**
+	 * Invokes the $closure with the aggregator and each item in
+	 * the matcher array and returns the final aggregated value
+	 *
+	 * @param  mixed    $aggregator
+	 * @param  Closure  $closure
+	 * @return mixed 
+	*/
 	public function reduce( $aggregator, $closure ){
-		foreach( $this->matchers as $item ){
+		$this->each( function( $item ) use ( &$aggregator, $closure ) {
 			$aggregator = $closure->__invoke( $aggregator, $item );
-		}
+		} );
 		return $aggregator;
 	}
 	
+	/**
+	 * Invokes the $closure with each item in the matcher array and 
+	 * and returns the first item when $closure returns a true value
+	 *
+	 * @param  Closure  $closure
+	 * @return mixed 
+	*/
 	public function find( $closure ){
 		foreach( $this->matchers as $item ){
 			$val = $closure->__invoke( $item );
@@ -55,50 +174,20 @@ class WPCOM_Platform_Redirector implements Iterator {
 		}
 	}
 	
+	/**
+	 * Invokes the $closure with each item in the matcher array and 
+	 * and returns an array of items where the $closure returned a
+	 * true value.
+	 *
+	 * @param  Closure  $closure
+	 * @return Array 
+	*/
 	public function findAll( $closure ){
-		$matches = array();
-		foreach( $this->matchers as $item ){
+		return $this->reduce( array(), function( $matches, $item ) use ( $closure ) {
 			$val = $closure->__invoke( $item );
 			if ( $val ) array_push( $matches, $item );
-		}
-		return $matches;
+			return $matches;
+		} );
 	}
-	
-	// Iterator methods
-	
-	/**
-	 * @return mixed
-	 */
-	public function current(){
-		return $this->matchers[$this->index];
-	}
-	
-	/**
-	 * @return scalar
-	 */
-	public function key(){
-		return $this->index;
-	}
-	
-	/**
-	 * @return void
-	 */
-	public function next(){
-		++ $this->index;
-	}
-	
-	/**
-	 * @return void
-	 */
-	public function rewind(){
-		$this->index = 0;
-	}
-	
-	/**
-	 * @return boolean
-	 */
-	public function valid(){
-		return isset($this->matchers[$this->position]);
-	}
-
+		
 }
--- Tests/WPCOM_Platform_RedirectorTest.php
+++ Tests/WPCOM_Platform_RedirectorTest.php
@@ -5,59 +5,117 @@ require 'Classes/WPCOM_Platform_Redirector.php';
 class WPCOM_Platform_RedirectorTest extends PHPUnit_Framework_TestCase {
 	
 	public function setUp(){
-		$this->instance = WPCOM_Platform_Redirector::getInstance();
-	}
-	
-	public function testInstanceIsSame(){
-		
-		$instance2 = WPCOM_Platform_Redirector::getInstance();
-		
-		$this->assertTrue( $this->instance === $instance2 );
+		$this->matcher = new WPCOM_Platform_Redirector();
+		$this->matcher
+			->on( function( $ua ){ return $ua == 'Awesomesauce Browser 2.1'; }, 'URL', 'LABEL' )
+			->on( function( $ua ){ return $ua == 'Awesomesauce Browser 2.0'; }, 'URL2', 'LABEL2');
 		
 	}
 	
-	public function testEnumerableMethods(){
+	public function testCompareString(){
+		
+		$this->matcher
+		->on( function( $ua ){
+			return preg_match( "/(ipad|iphone|ipod|ios)/i", $ua );
+		}, 'itms://somewhere', 'Open WordPress for iOS in the App Store' )
+		->on( "/android/i", 'android://something', 'Open WordPress for Android in the Google Play store' );
+		
+		extract( $this->matcher->matching( "Mozilla/5.0 (iPad; U; CPU OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5" ) );
+		
+		$this->assertSame( 'itms://somewhere', $href );
+		$this->assertSame( 'Open WordPress for iOS in the App Store', $label );
+		
+		extract( $this->matcher->matching( "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1" ) );
+		
+		$this->assertSame( 'android://something', $href );
+		$this->assertSame( 'Open WordPress for Android in the Google Play store', $label );
+		
+	}
+	
+	public function testPrepend(){
+		
+		$this->matcher->prepend( "/[\d]/", 'http://digits.com', 'Digits' );
+		extract( $this->matcher->matching( "Awesomesauce Browser 2.1" ) );
+		$this->assertSame( 'http://digits.com', $href );
+		$this->assertSame( 'Digits', $label );
+		
+	}
+		
+	public function testEach(){
 		
 		// look mom, it's chainable
-		$this->instance
-			->on( function( $ua ){
-				return $ua == 'Awesomesauce Browser 2.1';
-			}, 'URL', 'LABEL' )
-			->on( function( $ua ){
-				return $ua == 'Awesomesauce Browser 2.0';
-			}, 'URL2', 'LABEL2');
-		
-		$this->counter = 0;
-		$this->instance->each( function( $item ){
-			++ $this->counter;
+		$urls = array();
+		$this->matcher->each( function( $item ) use ( &$urls ){
+			array_push( $urls, $item['href'] );
 		});
-		$this->assertSame( 2, $this->counter );
+		
+		$this->assertSame( array( 'URL', 'URL2' ), $urls );
+		
+	}
+	
+	public function testMap(){		
 		
 		$this->assertSame( array( 'URL', 'URL2' ),
-			$this->instance->map( function( $item ){
+			$this->matcher->map( function( $item ){
 				return $item['href'];
 			} )
 		);
-
+	}
+	
+	public function testFind(){
+		
 		$this->assertSame( 'URL2',
-			$this->instance->find( function( $item ){
+			$this->matcher->find( function( $item ){
 				return preg_match( "/[\d]+/", $item['href'] );
 			} )['href']
 		);
 		
+	}
+	
+	public function testFindAll(){
+		
 		$this->assertSame( 'URL2',
-			$this->instance->findAll( function( $item ){
+			$this->matcher->findAll( function( $item ){
 				return preg_match( "/[\d]+/", $item['href'] );
 			} )[0]['href']
 		);
 		
+	}
+	
+	public function testReduce(){
+		
 		$this->assertSame( 7,
-			$this->instance->reduce( 0, function( $len, $item ){
+			$this->matcher->reduce( 0, function( $len, $item ){
 				return $len + strlen( $item['href'] );
 			} )
 		);
 		
+	}
+	
+	public function testInvokableObject(){
 		
+		$ua = 'Awesomesauce Browser 2.1' ;
+		
+		$this->matcher->prepend( new EveryOtherMatcher, 'http://maybe.com', 'Maybe' );
+		
+		$first = $this->matcher->matching( $ua );
+		$second = $this->matcher->matching( $ua );
+		$third = $this->matcher->matching( $ua );
+		
+		$this->assertSame( 'URL', $first['href'] );
+		$this->assertSame( 'http://maybe.com', $second['href'] );
+		$this->assertSame( $first, $third );
+	}
+	
+}
+
+class EveryOtherMatcher {
+	
+	private $is_true = 1;
+	
+	public function __invoke( $item ){
+		$this->is_true = $this->is_true * -1;
+		return $this->is_true > 0;
 	}
 	
 }
